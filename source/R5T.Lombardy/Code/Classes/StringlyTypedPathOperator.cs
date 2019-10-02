6b0e42b069d37e1ea52ba7e6197cda6fc8802e6b
MinexAutomation <minex.automation@gmail.com>
2019-10-02 13:43:56 -0700

    Updated path input argument names from "path part" to "path segment". Initial implementations of some operations.

--- source/R5T.Lombardy/Code/Classes/StringlyTypedPathOperator.cs
+++ source/R5T.Lombardy/Code/Classes/StringlyTypedPathOperator.cs
@@ -15,17 +15,17 @@ namespace R5T.Lombardy
             throw new NotImplementedException();
         }
 
-        public string CombineTwo(string directorySeparator, string pathPart1, string pathPart2)
+        public string CombineTwo(string directorySeparator, string pathSegment1, string pathSegment2)
         {
             throw new NotImplementedException();
         }
 
-        public string CombineTwo(string pathPart1, string pathPart2)
+        public string CombineTwo(string pathSegment1, string pathSegment2)
         {
             throw new NotImplementedException();
         }
 
-        public string CombineTwoUnresolved(string directorySeparator, string pathPart1, string pathPart2)
+        public string CombineTwoUnresolved(string directorySeparator, string pathSegment1, string pathSegment2)
         {
             throw new NotImplementedException();
         }
@@ -40,14 +40,16 @@ namespace R5T.Lombardy
         /// All input arguments are checked, but invalid inputs are not manipulated to ensure correctness, and instead exceptions are thrown.
         /// </summary>
         /// <param name="directorySeparator">The directory separator to use in combining paths. This value is checked to be one of the valid directory separators, and an exception is thrown if not.</param>
-        /// <param name="pathPart1">The first path part. This path part is checked to be non-directory indicated, and an exception is thrown if not.</param>
-        /// <param name="pathPart2">The second path part. This path part is checked to be non root-indicated, and an exception is thrown if not.</param>
-        public string CombineUnresolvedSimple(string directorySeparator, string pathPart1, string pathPart2)
+        /// <param name="pathSegment1">The first path part. This path part is checked to be non-directory indicated, and an exception is thrown if not.</param>
+        /// <param name="pathSegment2">The second path part. This path part is checked to be non root-indicated, and an exception is thrown if not.</param>
+        public string CombineUnresolvedSimple(string directorySeparator, string pathSegment1, string pathSegment2)
         {
+            DirectorySeparator.Validate(directorySeparator);
+            // Validate pathSegment1 is NOT directory indicated.
+            // Validate pathSegment2 is NOT root indicated.
 
-
-            var output = this.CombineUnresolvedUnchecked(directorySeparator, pathPart1, pathPart2);
-            return output;
+            var output = this.CombineUnresolvedUnchecked(directorySeparator, pathSegment1, pathSegment2);
+            return output; // No resolution of output.
         }
 
         /// <summary>
@@ -56,11 +58,11 @@ namespace R5T.Lombardy
         /// All inputs are unchecked, and the resulting path part may be unresolved.
         /// </summary>
         /// <param name="directorySeparator">The directory separator to use in combining paths. This value is not checked to be one of the valid directory separators.</param>
-        /// <param name="pathPart1">The first path part. This path part is not checked to be non-directory indicated. Multiple consecutive directory separator may result.</param>
-        /// <param name="pathPart2">The second path part. This path part is not checked to be non root-indicated. Multiple consecutive directory separator may result.</param>
-        public string CombineUnresolvedUnchecked(string directorySeparator, string pathPart1, string pathPart2)
+        /// <param name="pathSegment1">The first path part. This path part is not checked to be non-directory indicated. Multiple consecutive directory separator may result.</param>
+        /// <param name="pathSegment2">The second path part. This path part is not checked to be non root-indicated. Multiple consecutive directory separator may result.</param>
+        public string CombineUnresolvedUnchecked(string directorySeparator, string pathSegment1, string pathSegment2)
         {
-            var output = $"{pathPart1}{directorySeparator}{pathPart2}";
+            var output = $"{pathSegment1}{directorySeparator}{pathSegment2}";
             return output;
         }
     }
